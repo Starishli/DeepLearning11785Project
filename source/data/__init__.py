14bc603b44ef7e1a24ef0946a4f728f55e25aff9
invokerli <invokerli@outlook.com>
2020-04-07 19:42:14 -0400

    init commit

---
+++ source/data/__init__.py
@@ -0,0 +1,6 @@
+FEATURE_MAPPING = {"metabric1904": ["age", "sex", "race", "number of comorbidities",
+                                    "presence of diabetes", "presence of dementia",
+                                    "presence of cancer", "mean arterial blood pressure",
+                                    "heart rate", "respiration rate", "temperature", "white blood cell count",
+                                    "serums sodium", "serums creatinine"],
+                   }
