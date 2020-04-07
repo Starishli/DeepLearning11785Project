4853d2573d04c425b0565ff7792fbda3bc5a1dab
invokerli <invokerli@outlook.com>
2020-04-07 19:52:02 -0400

    add source dirs

---
+++ source/__init__.py
@@ -0,0 +1,7 @@
+import os
+SOURCE_DIR = os.path.dirname(os.path.abspath(__file__))
+
+DATA_DIR = os.path.join(SOURCE_DIR, 'data')
+RESULT_DIR = os.path.join(SOURCE_DIR, 'result')
+CACHE_DIR = os.path.join(SOURCE_DIR, 'cache')
+FIG_DIR = os.path.join(SOURCE_DIR, 'fig')
