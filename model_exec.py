45e60f5d8e9fe024d38fe7e965f7a23b8d828443
leyangzhang <leyangz@andrew.cmu.edu>
2020-04-08 16:37:39 -0400

    COX function

--- model_exec.py
+++ model_exec.py
@@ -1,11 +1,12 @@
 from source.model.RandomSurvivalForest import random_survival_forest
 from source.model.Cox import cox
 from source.model.DeepSurv import deepsurv_trainNtest
-
+from source.model.Cox import cox
 
 if __name__ == "__main__":
     dataset_name = "metabric"
 
+    cox_score = cox(dataset_name)
     rsf_score = random_survival_forest(dataset_name)
     deepsurv_score = deepsurv_trainNtest(dataset_name)
 
